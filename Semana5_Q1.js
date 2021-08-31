1d0166bae68aa5d7200176d46a0d5d56fb7271e3
Hacjesse <89802664+Hacjesse@users.noreply.github.com>
2021-08-31 11:33:55 -0300

    Add files via upload

--- Semana5_Q1.js
+++ Semana5_Q1.js
@@ -1,1 +1,15 @@
+function createList(quantidadeItens, mensagem) {
+  let i = 0;
+  let ul = document.querySelector('ul');
 
+  while (i < quantidadeItens) {
+    var li = document.createElement('li');
+    var texto = document.createTextNode(mensagem);
+    li.appendChild(texto);
+    ul.appendChild(li);
+    i++;
+  }
+}
+let qtdItens = parseInt(prompt('Quantos intens serão exibidos? '));
+let mensagem = prompt('Qual a mensagem ?');
+createList(qtdItens, mensagem);
